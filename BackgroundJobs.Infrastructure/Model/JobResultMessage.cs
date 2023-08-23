670fe7cc7d2f70114511090f8532691f8c63881a
Facundo Muñoz <facurmz@icloud.com>
2023-08-23 16:04:08 -0300

    Added integration with SNS
    - Now the results are published to an SNS topic.

---
+++ BackgroundJobs.Infrastructure/Model/JobResultMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace BackgroundJobs.Infrastructure.Model;
+
+public class JobResultMessage
+{
+    public required string JobKey { get; init; }
+    public required string Status { get; init; }
+}
